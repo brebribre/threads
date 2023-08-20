07483bc00db2d357ef9fe6487305eafb397e54db
brebribre <70563428+brebribre@users.noreply.github.com>
2023-08-20 18:59:17 +0200

    implements topbar,sidebar, login with clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css';
+
+//specify different rules for auth routes
+export const metadata = {
+    title: 'Threads',
+    description : 'A Next.js 13 Threads application from Meta'
+}
+
+//set default font
+const inter = Inter({ subsets: ["latin"]})
+
+//CLERK Authentication
+export default function RootLayout( {
+    children
+}: {
+    children: React.ReactNode
+} ){
+    return (
+    <ClerkProvider>
+        <html lang = "en">
+            <body className={'${inter.className} bg-dark-1'}>
+
+            </body>
+        </html>
+    </ClerkProvider>
+    )
+}
---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,5 @@
+function Bottombar(){
+    return <h1>Bottom Bar</h1>
+}
+
+export default Bottombar;
